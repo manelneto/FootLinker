a6842adc5ff782f3c6576769edd33f771678de99
Manuel Neto <92862229+manelneto@users.noreply.github.com>
2023-03-23 10:28:04 +0000

    added match fetcher

---
+++ app/lib/controller/match_fetcher.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+import '../model/match.dart';
+
+class MatchFetcher {
+  Future<Match> fetchMatch() async {
+    final response = await http.get(
+        Uri.parse('https://v3.football.api-sports.io/teams?id=239625'), // FC Porto (id = 212)
+        headers: {
+          'x-rapidapi-key': '28e2bd7ea34da73c2eb4e946fe77c052',
+          'x-rapidapi-host': 'v3.football.api-sports.io',
+        }
+    );
+
+    if (response.statusCode == 200) {
+      return Match.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception(response.reasonPhrase);
+    }
+  }
+}
